e81e17c56ffd3accfb47c8e8e97543d0a9666804
julien.beaussier <julien.beaussier@c4b00fdc-2b61-11de-9b0f-33ff132f608d>
2009-08-12 21:32:11 +0000

    Choix du fichier pour la sauvegarde et l'enregistrement

--- trunk/FrigoIntelligent/src/FileAction.java
+++ trunk/FrigoIntelligent/src/FileAction.java
@@ -1,8 +1,10 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
@@ -42,7 +44,11 @@ public class FileAction implements ActionListener {
 		// Sauvegarde dans le fichier xml
 		else if( ((JMenuItem)(e.getSource())).getText().equals("In file") ) {
 			try {
-				XMLManager.encodeToFile(m_oSmartFridge.m_oSmartFridge, "SmartFridge_db.xml");
+				JFileChooser oFC = new JFileChooser();
+				oFC.showSaveDialog(m_oSmartFridge.m_oApp);
+				File fSelected = oFC.getSelectedFile();
+				if( fSelected != null )
+					XMLManager.encodeToFile(m_oSmartFridge.m_oSmartFridge, fSelected.getName() );
 			} catch (FileNotFoundException e1) {
 				e1.printStackTrace();
 			} catch (IOException e1) {
@@ -53,7 +59,11 @@ public class FileAction implements ActionListener {
 		// Chargement depuis le fichier xml
 		else if( ((JMenuItem)(e.getSource())).getText().equals("From file") ) {
 			try {
-				m_oSmartFridge.m_oSmartFridge  = (SmartFridge) XMLManager.decodeFromFile("SmartFridge_db.xml");
+				JFileChooser oFC = new JFileChooser();
+				oFC.showOpenDialog(m_oSmartFridge.m_oApp);
+				File fSelected = oFC.getSelectedFile();				
+				m_oSmartFridge.m_oSmartFridge  = (SmartFridge) XMLManager.decodeFromFile(fSelected.getName());
+				m_oSmartFridge.m_oApp.listRecipeAction();
 			} catch (FileNotFoundException e1) {
 				System.out.println("Le fichier SmartFridge_db.xml n'existe pas");
 			} catch (IOException e1) {
