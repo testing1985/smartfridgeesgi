1380ee5010c7931806b58beb0a1ab01aa2536f43
julien.beaussier <julien.beaussier@c4b00fdc-2b61-11de-9b0f-33ff132f608d>
2009-08-13 20:43:36 +0000

    Loading depuis internet

--- trunk/FrigoIntelligent/src/Session.java
+++ trunk/FrigoIntelligent/src/Session.java
@@ -1,5 +1,6 @@
 import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -43,6 +44,60 @@ public class Session {
 		return m_bIsConnected;
 	}
 	
+	public void load( SmartFridge oSF ) {
+		Connection oBDD = DBConnectionManager.getInstance().getConnection();
+				
+		try {
+			Statement oST = oBDD.createStatement();
+			String sGetRecipe = "SELECT * FROM Recipe WHERE userRecipe = " + m_iID;
+			ResultSet oRS = oST.executeQuery( sGetRecipe );
+			while( oRS.next() ) {
+				Recipe oRecipe = new Recipe();
+				int idRecipe = oRS.getInt("idRecipe");
+				oRecipe.setName( oRS.getString("nameRecipe") );
+				oRecipe.setType( oRS.getString("typeRecipe") );
+				Vector<RecipeStage> vStages = new Vector<RecipeStage>();
+				Vector<Aliment> vAliments = new Vector<Aliment>();
+				
+				Statement oST2 = oBDD.createStatement();
+				String sGetAliments = "SELECT * FROM Aliment WHERE recipeAliment = " + idRecipe + " AND userAliment = " + m_iID;
+				ResultSet oAlimentRS = oST2.executeQuery( sGetAliments );
+				while( oAlimentRS.next() ) {
+					Aliment oAliment = new Aliment();
+					oAliment.setName( oAlimentRS.getString("nameAliment") );
+					oAliment.setQuantity( oAlimentRS.getInt("quantityAliment") );
+					oAliment.setUnite( oAlimentRS.getString("uniteAliment") );
+					vAliments.add( oAliment );
+				}
+				
+				String sGetStages = "SELECT * FROM RecipeStage WHERE recipeStage = " + idRecipe;
+				ResultSet oStagesRS = oST2.executeQuery( sGetStages );
+				oStagesRS.last();
+				int iNbStages = oStagesRS.getRow();
+				oStagesRS.beforeFirst();
+				vStages.setSize( iNbStages );
+				
+				while( oStagesRS.next() ) {
+					RecipeStage oStage = new RecipeStage();
+					oStage.setDescription( oStagesRS.getString("descStage") );
+					oStage.setDifficulty( oStagesRS.getInt("difficultyStage") );
+					oStage.setTime( oStagesRS.getInt("timeStage") );
+					vStages.setElementAt(oStage, oStagesRS.getInt("idStage") - 1 );
+				}
+				
+				oRecipe.setAliments(vAliments);
+				oRecipe.setRecipeStages(vStages);
+				
+				oSF.addRecipe( oRecipe );
+			}
+			
+		} catch ( SQLException e ) {
+			e.printStackTrace();
+		}
+		
+		DBConnectionManager.getInstance().closeConnection();
+	}
+	
 	public void save( SmartFridge oSF ) {
 		Connection oBDD = DBConnectionManager.getInstance().getConnection();
 		
@@ -79,37 +134,40 @@ public class Session {
 			
 			for( int i = 0 ; i < vRecipes.size() ; i++ ) {
 				Recipe oRecipe = vRecipes.elementAt(i);
-				String sAddRecipe = "" +
-					" INSERT INTO Recipe ( userRecipe , nameRecipe , typeRecipe )" +
-					" VALUES( " + m_iID + " , '"+ oRecipe.getName() +"' , '"+ oRecipe.getType() +"' )";
-				oSt.executeUpdate( sAddRecipe , Statement.RETURN_GENERATED_KEYS );
-				ResultSet rsKey = oSt.getGeneratedKeys();
+				String sAddRecipe = "INSERT INTO Recipe ( userRecipe , nameRecipe , typeRecipe ) VALUES( ?,?,? )";
+				PreparedStatement oPST = oBDD.prepareStatement( sAddRecipe , Statement.RETURN_GENERATED_KEYS );
+				oPST.setShort (1, m_iID);
+				oPST.setString(2, oRecipe.getName());
+				oPST.setString(3, oRecipe.getType());
+				oPST.executeUpdate();
+				ResultSet rsKey = oPST.getGeneratedKeys();
 				if( rsKey.next() ) {
 					int idRecipe = rsKey.getInt(1);
 					
 					Vector<Aliment> vAliments = oRecipe.getAliments();
 					for( int j = 0 ; j < vAliments.size() ; j++ ) {
 						Aliment oAliment = vAliments.elementAt(j);
-						String sAddAliment = "" +
-							" INSERT INTO Aliment ( userAliment , recipeAliment , nameAliment , quantityAliment , priceAliment , uniteAliment , peremptionAliment ) " +
-							" VALUES( "  +
-								m_iID    + "," + 
-								idRecipe + "," +
-								"'" + oAliment.getName()  + "'," + 
-								oAliment.getQuantity()    + ","  +
-								oAliment.getPrice() 	  + ","  +
-								"'" + oAliment.getUnite() + "'," +
-								oAliment.getPeremption()  +
-							")";
-						oSt.executeUpdate( sAddAliment );
+						String sAddAliment = "INSERT INTO Aliment ( userAliment , recipeAliment , nameAliment , quantityAliment , uniteAliment ) VALUES (?,?,?,?,?)";
+						oPST = oBDD.prepareStatement(sAddAliment);
+						oPST.setShort (1, m_iID);
+						oPST.setInt   (2, idRecipe);
+						oPST.setString(3, oAliment.getName());
+						oPST.setInt   (4, oAliment.getQuantity());
+						oPST.setString(5, oAliment.getUnite());
+						oPST.executeUpdate();
 					}
 					
 					Vector<RecipeStage> vStages = oRecipe.getRecipeStages();
 					for( int j = 0 ; j < vStages.size() ; j++ ) {
 						RecipeStage oStage = vStages.elementAt(j);
-						String sAddStage = "" +
-								"INSERT INTO RecipeStage VALUES("+(j+1)+" , "+idRecipe+" , "+oStage.getTime()+" , "+oStage.getDifficulty()+" , '"+oStage.getDescription()+"')";
-						oSt.executeUpdate( sAddStage );
+						String sAddStage = "INSERT INTO RecipeStage ( idStage , recipeStage , timeStage , difficultyStage , descStage ) VALUES(?,?,?,?,?)";
+						oPST = oBDD.prepareStatement(sAddStage);
+						oPST.setInt(1, j+1);
+						oPST.setInt(2, idRecipe);
+						oPST.setInt(3, oStage.getTime());
+						oPST.setInt(4, oStage.getDifficulty());
+						oPST.setString(5, oStage.getDescription());
+						oPST.executeUpdate();
 					}
 					
 				}
